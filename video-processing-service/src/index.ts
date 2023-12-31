5e80f745d8c60ef4858460f701cbbc5ab84cec84
Brian Tran <exobrian@yahoo.com>
2023-12-31 11:05:28 -0800

    https://neetcode.io/courses/full-stack-dev/6

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,73 @@
 import express from "express";
+import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setUpDirectories, uploadProcessedVideo } from "./storage";
+
+setUpDirectories();
 
 const app = express();
-const port = 3000;
 
+// Set app to use jsons
+app.use(express.json());
+
+// Get Request: Home
 app.get("/", (req, res) => {
     res.send("Hello! I'm working.");
 })
 
+// Post Request - this must be async as there is a specific order we need to follow.
+app.post("/process-video", async (req, res) => {
+    // Get bucket and filename from the Cloud Pub/Sub message
+    let data;
+
+    // Standard way of taking message from pub/sub messaging queue
+    // Here, we take the message and parse it into data. 
+    try {
+        const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+        data = JSON.parse(message);
+        if (!data.name) {
+            throw new Error(`Invalid message received.`);
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send(`Bad Request: missing filename`);
+    }
+
+    const inputFileName = data.name;
+    const outputFileName = `processed-${inputFileName}`;
+
+    // 1. Download the raw video from cloud storage. Need to wait on this before continuing.
+    await downloadRawVideo(inputFileName);
+
+    // 2. Convert the raw video.
+    try {
+        await convertVideo(inputFileName, outputFileName);
+    } catch(error) {
+        // If there is an error, delete the files as they are corrupt.
+        // We can join multiple promises into a single array and make all asynchronous.
+        await Promise.all([
+            deleteRawVideo(inputFileName),
+            deleteProcessedVideo(outputFileName)
+        ]);
+
+        console.error(error);
+        return res.status(500).send("Internal Server Error: video processing failed.");
+    }
+
+    // 3. Upload the converted video to cloud storage.
+    await uploadProcessedVideo(outputFileName);
+    
+    // 4. Now that we're successful, delete the files to save space.
+    // We can join multiple promises into a single array and make all asynchronous.
+    await Promise.all([
+        deleteRawVideo(inputFileName),
+        deleteProcessedVideo(outputFileName)
+    ]);
+    return res.status(200).send("Processing finished successfully.");
+})
+
+// Set env variable for port in prod
+const port = process.env.PORT || 3000;
+
+// Start app server
 app.listen(port, () => {
     console.log(`Video Processing Service listening on http://localhost:${port}`);
 })
---
+++ video-processing-service/src/storage.ts
@@ -0,0 +1,132 @@
+import { Storage } from '@google-cloud/storage';
+import fs from 'fs';
+import ffmpeg from 'fluent/ffmpeg';
+
+const storage = new Storage();
+
+const rawVideoBucketName = 'bt-yt-raw-videos';
+const processedVideoBucketName = 'bt-yt-processed-videos';
+
+const localRawVideoPath = './raw-videos';
+const localProcessedVideoPath = './processed-videos';
+
+/* 
+* Function to setup local directories for raw and processed files
+*/
+export function setUpDirectories(){
+    ensureDirectoryExistence(localRawVideoPath);
+    ensureDirectoryExistence(localProcessedVideoPath);
+}
+
+/**
+ * 
+ * @param rawVideoName - The name of the file to convert from {@link localRawVideoPath}.
+ * @param processedVideoName - The name of the file to convert to {@link localProcessedVideoPath}.
+ * @returns A promise that resolves when the video has been processed.
+ */
+export function convertVideo(rawVideoName: string, processedVideoName: string){
+    return new Promise<void>((resolve, reject) => {
+        // Convert video by builder pattern
+        ffmpeg(`${localRawVideoPath}/${rawVideoName}`)
+        .outputOptions("-vf", "scale=-1:360")
+        .on("end", () => {
+            console.log("Video processed successfully!")
+            resolve();
+        })
+        .on("error", (err) => {
+            console.log(`An error has occurred: ${err.message}`);
+            reject(err);
+        })
+        .save(`${localProcessedVideoPath}/${processedVideoName}`);
+    })
+}
+
+/**
+ * 
+ * @param fileName - The name of the file to download from the
+ * {@link rawVideoBucketName} bucket into the {@link localRawVideoPath} folder.
+ * @returns A promise that resolves when the file has been downloaded.
+ */
+export async function downloadRawVideo(fileName: string){
+    await storage.bucket(rawVideoBucketName)
+        .file(fileName)
+        .download({destination: `${localRawVideoPath}/${fileName}`})
+
+    console.log(
+        `gs://${localRawVideoPath}/${fileName} downloaded to ${localProcessedVideoPath}/${fileName}.`
+    )
+}
+
+/**
+ * 
+ * @param fileName - The name of the file to upload from the {@link localProcessedVideoPath}
+ * folder into the {@link processedVideoBucketName}.
+ * @returns A promise that resolves when the file has been uploaded.
+ */
+export async function uploadProcessedVideo(fileName: string){
+    const bucket = storage.bucket(processedVideoBucketName);
+    await bucket.upload(`${processedVideoBucketName}/${fileName}`, {
+        destination: fileName
+    });
+
+    console.log(`${localProcessedVideoPath}/${fileName} uploaded to gs://${processedVideoBucketName}/${fileName}`);
+
+    await bucket.file(fileName).makePublic();
+}
+
+/**
+ * 
+ * @param fileName - The name of the file to delete from the {@link localRawVideoPath} folder.
+ * @returns A promise that resolves when the file has been deleted.
+ */
+export function deleteRawVideo(fileName: string){
+    return deleteFile(`${localRawVideoPath}/${fileName}`);
+}
+
+/**
+ * 
+ * @param fileName - The name of the file to delete from the {@link localProcessedVideoPath} folder.
+ * @returns A promise that resolves when the file has been deleted.
+ */
+export function deleteProcessedVideo(fileName: string){
+    return deleteFile(`${localProcessedVideoPath}/${fileName}`);
+}
+
+/**
+ * 
+ * @param filePath - The full file path to delete
+ * @returns A promise that resolves when the file has been deleted.
+ */
+function deleteFile(filePath: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        if (fs.existsSync(filePath)){
+            // Efficient method to delete files
+            fs.unlink(filePath, (err) => {
+                if (err) {
+                    console.log(`Failed to delete file at ${filePath}.`, err);
+                    // Using resolve here. May want to consider using reject if visibility necessary.
+                    resolve();
+                } else {
+                    console.log(`File deleted at ${filePath}`);
+                    resolve();
+                }
+            })
+        } else {
+            console.log(`File not found at ${filePath}. Skipping delete process.`);
+            // Using resolve here. May want to consider using reject if visibility necessary.
+            resolve();
+        }
+    })
+}
+
+/**
+ * Ensures a directory exists, creating it recursively if necessary.
+ * @param dirPath - The full directory path to check.
+ */
+function ensureDirectoryExistence(dirPath: string){
+    // Create directory recursively if it doesn't already exist
+    if (!fs.existsSync(dirPath)) {
+        fs.mkdirSync(dirPath, {recursive:true});
+        console.log(`Directory created at ${dirPath}`);
+    }
+}
